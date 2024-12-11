0eabbc6207a55a5b699bc1e13a69c7864b0cb6f2
FOwen123 <filbertowen.s@gmail.com>
2024-12-11 11:59:35 +0800

    add changes

--- DAOlingo/anchor/tests/DAOlingo.spec.ts
+++ DAOlingo/anchor/tests/DAOlingo.spec.ts
@@ -1,76 +1,16 @@
-import * as anchor from '@coral-xyz/anchor'
-import {Program} from '@coral-xyz/anchor'
-import {Keypair} from '@solana/web3.js'
-import {DAOlingo} from '../target/types/DAOlingo'
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { DAOlingo } from "../target/types/DAOlingo";
 
-describe('DAOlingo', () => {
+describe("daolingo", () => {
   // Configure the client to use the local cluster.
-  const provider = anchor.AnchorProvider.env()
-  anchor.setProvider(provider)
-  const payer = provider.wallet as anchor.Wallet
+  anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.DAOlingo as Program<DAOlingo>
+  const program = anchor.workspace.DAOlingo as Program<DAOlingo>;
 
-  const DAOlingoKeypair = Keypair.generate()
-
-  it('Initialize DAOlingo', async () => {
-    await program.methods
-      .initialize()
-      .accounts({
-        DAOlingo: DAOlingoKeypair.publicKey,
-        payer: payer.publicKey,
-      })
-      .signers([DAOlingoKeypair])
-      .rpc()
-
-    const currentCount = await program.account.DAOlingo.fetch(DAOlingoKeypair.publicKey)
-
-    expect(currentCount.count).toEqual(0)
-  })
-
-  it('Increment DAOlingo', async () => {
-    await program.methods.increment().accounts({ DAOlingo: DAOlingoKeypair.publicKey }).rpc()
-
-    const currentCount = await program.account.DAOlingo.fetch(DAOlingoKeypair.publicKey)
-
-    expect(currentCount.count).toEqual(1)
-  })
-
-  it('Increment DAOlingo Again', async () => {
-    await program.methods.increment().accounts({ DAOlingo: DAOlingoKeypair.publicKey }).rpc()
-
-    const currentCount = await program.account.DAOlingo.fetch(DAOlingoKeypair.publicKey)
-
-    expect(currentCount.count).toEqual(2)
-  })
-
-  it('Decrement DAOlingo', async () => {
-    await program.methods.decrement().accounts({ DAOlingo: DAOlingoKeypair.publicKey }).rpc()
-
-    const currentCount = await program.account.DAOlingo.fetch(DAOlingoKeypair.publicKey)
-
-    expect(currentCount.count).toEqual(1)
-  })
-
-  it('Set DAOlingo value', async () => {
-    await program.methods.set(42).accounts({ DAOlingo: DAOlingoKeypair.publicKey }).rpc()
-
-    const currentCount = await program.account.DAOlingo.fetch(DAOlingoKeypair.publicKey)
-
-    expect(currentCount.count).toEqual(42)
-  })
-
-  it('Set close the DAOlingo account', async () => {
-    await program.methods
-      .close()
-      .accounts({
-        payer: payer.publicKey,
-        DAOlingo: DAOlingoKeypair.publicKey,
-      })
-      .rpc()
-
-    // The account should no longer exist, returning null.
-    const userAccount = await program.account.DAOlingo.fetchNullable(DAOlingoKeypair.publicKey)
-    expect(userAccount).toBeNull()
-  })
-})
+  it("Is initialized!", async () => {
+    // Add your test here.
+    const tx = await program.methods.initialize().rpc();
+    console.log("Your transaction signature", tx);
+  });
+});
